b014675b605101d93b67b7e04e617539ba3b622e
gutNorbert <g.norby21@gmail.com>
2018-02-07 22:59:45 +0100

    Added ListView

--- app/src/main/java/com/gutNorbert/flashchatnewfirebase/ChatListAdapter.java
+++ app/src/main/java/com/gutNorbert/flashchatnewfirebase/ChatListAdapter.java
@@ -1,8 +1,84 @@
 package com.gutNorbert.flashchatnewfirebase;
 
+import android.app.Activity;
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 /**
  * Created by Norbi on 2018. 02. 07..
  */
 
-public class ChatListAdapter {
+public class ChatListAdapter extends BaseAdapter{
+
+    private Activity mActivity;
+    private DatabaseReference mDatabaseReference;
+    private String mDisplayName;
+    private ArrayList<DataSnapshot> mSnapshotList;
+
+    public ChatListAdapter(Activity activity, DatabaseReference databaseReference, String displayName) {
+        mActivity = activity;
+        mDatabaseReference = databaseReference.child("messages");
+        mDisplayName = displayName;
+        mSnapshotList = new ArrayList<>();
+    }
+
+    static class ViewHolder{
+        TextView authorName;
+        TextView body;
+        LinearLayout.LayoutParams params;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public InstantMessage getItem(int pos) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int pos, View view, ViewGroup viewGroup) {
+        if (view == null){
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.chat_msg_row, viewGroup, false);
+            final ViewHolder holder = new ViewHolder();
+            holder.authorName = (TextView) view.findViewById(R.id.author);
+            holder.body = (TextView) view.findViewById(R.id.message);
+            holder.params = (LinearLayout.LayoutParams) holder.authorName.getLayoutParams();
+            view.setTag(holder); //Temporarly store the viewholder
+
+        }
+
+        final InstantMessage message = getItem(pos);
+        final ViewHolder holder = (ViewHolder)view.getTag();
+
+        String author = message.getAuthor();
+        holder.authorName.setText(author);
+
+        String msg = message.getMessage();
+        holder.body.setText(msg);
+
+        return view;
+    }
 }
